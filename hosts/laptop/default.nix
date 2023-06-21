92cb4042f6eb89a24ddb5926f726d0013ab1ff72
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-06-21 20:51:58 +0200

    auto push rebuild laptop config

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -257,6 +257,21 @@
      feh
      polybar
      gnome.gnome-screenshot
+     # support both 32- and 64-bit applications
+    wineWowPackages.stable
+
+    # support 32-bit only
+    wine
+
+    # support 64-bit only
+    (wine.override { wineBuild = "wine64"; })
+
+    # wine-staging (version with experimental features)
+    wineWowPackages.staging
+
+    # winetricks (all versions)
+    winetricks
+
   ];
 
   services.blueman.enable = true;
