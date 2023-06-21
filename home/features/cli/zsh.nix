40b7f6cf19c67ad7b067e067db2ec1a69cc2b6db
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-06-21 16:01:43 +0200

    auto push rebuild laptop config

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -9,7 +9,7 @@
       };
       shellAliases = {
         sysfetch = "neofetch --ascii /home/mrtuxa/.ascii";
-        rebuild = "cd /home/mrtuxa/todotfiles && sudo nixos-rebuild switch --flake .#laptop --cores 8 --fast --upgrade -j 8 && git add . && git commit --allow-empty -m 'auto push rebuild laptop config' && git push ssh";
+        rebuild = "cd /home/mrtuxa/todotfiles && nix flake check && sudo nixos-rebuild switch --flake .#laptop --cores 8 --fast --upgrade -j 8 && git add . && git commit --allow-empty -m 'auto push rebuild laptop config' && git push ssh";
 	playsoundcloud = "mpv '$(youtube-dl -g $1)'";
 	};
     };
